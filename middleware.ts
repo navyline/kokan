426cad90d0d7a7df3ce9f27e5f136b55b325e0fb
navyline <nattakorn3667@gmail.com>
2025-01-18 22:28:59 +0700

    22222

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,6 @@ import { clerkMiddleware } from '@clerk/nextjs/server';
 export default clerkMiddleware();
 
 export const config = {
-  runtime: "nodejs", // ใช้ Node.js Runtime
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',

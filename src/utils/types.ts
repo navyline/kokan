4d0b3eccab4b50a2af1135893ec6c94b37869dab
navyline <nattakorn3667@gmail.com>
2025-02-03 20:28:34 +0700

    0.0.0e

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -44,3 +44,29 @@ export type Category = {
 };
 
 export type TradeStatus = "PENDING" | "ACCEPTED" | "REJECTED" | "CANCELLED" | "COMPLETED";
+
+export type actionFunction = (
+  prevState: Record<string, unknown>,
+  formData: FormData
+) => Promise<{ message: string }>;
+
+export type LandmarkCardProps = {
+  id: string;
+  name: string;
+  description: string;
+  image: string;
+  category: string;
+  province: string;
+  price: number;
+  lat: number;
+  lng: number;
+};
+
+export type CurrentSlideData = {
+  data: LandmarkCardProps;
+  index: number;
+};
+// export type CurrentSlideData = {
+//   data: LandmarkCardProps;
+//   // index: number;
+// };
